9e230be1652f6a4c2079cb7f1c714bc3ca08bffb
Giancarlo Bellido <giancarlo@server.(none)>
2008-11-28 14:02:12 -0500

    Added ruvim/api.rb

---
+++ lib/ruvim/api.rb
@@ -0,0 +1,41 @@
+#
+# API - Ruvim
+#
+
+module Ruvim
+
+	module API
+
+		ESCAPE = 27
+		RETURN = 13
+		DELETE = Curses::Key::DC
+		CR = "\n"
+
+		# Returns current buffer
+		def buffer
+			editor.buffer
+		end
+
+		# Exits Application and Closes Screen
+		def exit
+			raise RuvimExit
+		end
+		alias_method :q, :exit
+
+		# Returns current line
+		def line
+			editor.buffer.line.to_s
+		end
+
+		# Returns Current char
+		def char
+			editor.buffer.char
+		end
+
+		def cursor
+			editor.cursor
+		end
+
+	end
+
+end
