40c4a88e2d39ea203519123219ae2337475fe7cf
Giancarlo Bellido <giancarlo@server.(none)>
2008-12-24 14:12:53 -0500

    Removed Ruvim/Core

--- bin/ruvim.rb
+++ bin/ruvim.rb
@@ -5,7 +5,22 @@
 #
 
 require 'ruvim/curses'
-require 'ruvim/core'
+require 'ruvim/version'
+require 'ruvim/window'
+require 'ruvim/cursor'
+require 'ruvim/line'
+require 'ruvim/plugin'
+require 'ruvim/error'
+require 'ruvim/modes'
+require 'ruvim/page'
+require 'ruvim/editor'
+require 'ruvim/bindings'
+require 'ruvim/buffer'
+require 'ruvim/mappings'
+require 'ruvim/io'
+require 'ruvim/movement'
+require 'ruvim/message'
+require 'ruvim/api'
 
 module Ruvim
 
