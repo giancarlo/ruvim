df65f60674b15950c15cfb23bd9436afb7d0eded
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2010-05-17 19:35:48 -0400

    Fixed blank file Coderay Issue and COloring

---
+++ test/colors.rb
@@ -0,0 +1,16 @@
+require 'curses';
+
+Curses.init_screen
+Curses.start_color
+Curses.use_default_colors
+
+Curses.colors.times do |clr|
+	Curses.init_pair(clr, 1, clr)
+	Curses.attron Curses.color_pair clr
+	Curses.addstr(" ")
+	Curses.attroff Curses.color_pair clr
+end
+
+Curses.refresh
+
+Curses.getch
